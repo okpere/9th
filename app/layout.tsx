ea854f83d8aa66920c10cec7a0ac3cb2e7f66134
Clinton Okpere <clintonokpere99@gmail.com>
2025-05-03 16:43:04 +0100

    feat: clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import GlobalLayout from '@/components/layout';
 import { Lato } from 'next/font/google';
@@ -28,6 +27,8 @@ export default function RootLayout({
     <html lang='en'>
       <head>
         {/* Add any additional <meta> tags here if needed */}
+        <link rel='icon' type='image/svg+xml' href='/iconNow.svg' />
+
         <Script
           id='tawk-to'
           strategy='afterInteractive'
